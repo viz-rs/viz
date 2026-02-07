e9b680e6532679cf4e69143b6544153094905f52
Fangdun Tsai <fundon@pindash.io>
2026-02-07 13:50:16 +0800

    makepad: remove makepad-vector dep

--- crates/makepad_tessellation/src/utils.rs
+++ crates/makepad_tessellation/src/utils.rs
@@ -1,4 +1,4 @@
-use makepad_vector::geometry::Point;
+use makepad_widgets::Vec2d;
 
 /// Converts a type to another type.
 pub trait Convert<T> {
@@ -12,8 +12,11 @@ impl<const N: usize, O, I: Convert<O>> Convert<[O; N]> for [I; N] {
     }
 }
 
-impl Convert<Point> for lyon_path::math::Point {
-    fn convert(self) -> Point {
-        Point::new(self.x as f64, self.y as f64)
+impl Convert<Vec2d> for lyon_path::math::Point {
+    fn convert(self) -> Vec2d {
+        Vec2d {
+            x: self.x as f64,
+            y: self.y as f64,
+        }
     }
 }
--- crates/makepad_tessellation/src/vertex.rs
+++ crates/makepad_tessellation/src/vertex.rs
@@ -1,28 +1,28 @@
 use lyon_tessellation::{
     self as tess, FillVertex, FillVertexConstructor, StrokeVertex, StrokeVertexConstructor,
 };
-use makepad_vector::geometry::Point;
+use makepad_widgets::Vec2d;
 
 use crate::utils::Convert;
 
 /// The index type of a mesh.
 type IndexType = u32;
 /// Lyon's [`VertexBuffers`] generic data type defined for [`Point`].
-pub type VertexBuffers = tess::VertexBuffers<Point, IndexType>;
+pub type VertexBuffers = tess::VertexBuffers<Vec2d, IndexType>;
 
 /// Zero-sized type used to implement various vertex construction traits from Lyon.
 pub struct VertexConstructor;
 
 /// Enables the construction of a [`Point`] when using a `FillTessellator`.
-impl FillVertexConstructor<Point> for VertexConstructor {
-    fn new_vertex(&mut self, vertex: FillVertex) -> Point {
+impl FillVertexConstructor<Vec2d> for VertexConstructor {
+    fn new_vertex(&mut self, vertex: FillVertex) -> Vec2d {
         vertex.position().convert()
     }
 }
 
 /// Enables the construction of a [`Point`] when using a `StrokeTessellator`.
-impl StrokeVertexConstructor<Point> for VertexConstructor {
-    fn new_vertex(&mut self, vertex: StrokeVertex) -> Point {
+impl StrokeVertexConstructor<Vec2d> for VertexConstructor {
+    fn new_vertex(&mut self, vertex: StrokeVertex) -> Vec2d {
         vertex.position().convert()
     }
 }
